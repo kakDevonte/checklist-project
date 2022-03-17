d77dac0aa24318070b253d6f3e38ca192c9236da
kakDevonte <kakdevonte@gmail.com>
2022-03-17 22:46:42 +0700

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {AuthPage} from './components/AuthPage'
+import {Navigate, Route, Routes} from 'react-router-dom'
+import React, {useEffect} from 'react'
+import {Spinner} from 'react-bootstrap'
+import {useDispatch, useSelector} from 'react-redux'
+import {AdminWrapper} from './components/AdminPage/AdminWrapper'
+import {HeadWrapper} from './components/HeadPage/HeadWrapper'
+import {UserWrapper} from './components/UserPage/UserWrapper'
+import {ListWrapper} from './components/AdminPage/ListWrapper'
+import {Users} from './components/AdminPage/Users'
+import {CreateEditList} from './components/AdminPage/CreateEditList'
+import {initializeApp} from './redux/actions/app-action'
+import {CreateEditUser} from './components/AdminPage/CreateEditUser'
+import {UsersWrapper} from './components/AdminPage/UsersWrapper'
+import {UserViewListWrapper} from './components/UserPage/UserViewListWrapper'
+import {FormToFill} from './components/UserPage/FormToFill'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const dispatch = useDispatch()
+    const {initialized} = useSelector(state => state.app)
+
+    useEffect(() => {
+        dispatch(initializeApp())
+    }, [dispatch])
+
+    return <>
+        {!initialized
+            ? <div className="d-flex justify-content-center"><Spinner animation="border"/></div>
+            : <Routes>
+                <Route path="/admin" element={<AdminWrapper/>}>
+                    <Route index element={<ListWrapper/>}/>
+                    <Route path="tool/*" element={<CreateEditList/>}/>
+                    <Route path="users" element={<UsersWrapper/>}>
+                        <Route index element={<Users/>}/>
+                        <Route path="editor/*" element={<CreateEditUser/>}/>
+                    </Route>
+                </Route>
+                <Route path="/head" element={<HeadWrapper/>}/>
+                <Route path="/user" element={<UserWrapper/>}>
+                    <Route index element={<UserViewListWrapper/>}/>
+                    <Route path="form/*" element={<FormToFill/>}/>
+                </Route>
+                <Route path="/login" element={<AuthPage/>}/>
+                <Route path="/" element={<Navigate from="/" to="/login"/>}/>
+        </Routes>}
+    </>
 }
 
-export default App;
+export default App
